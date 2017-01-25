dde071ad28eebfe47bed0202e36ce8b713521f24
Nishant Sahoo <nishantsahoo.in@gmail.com>
2017-01-25 10:18:16 +0530

    Initial commit

---
+++ DataMap_IMDB_Top250_2016.py
@@ -0,0 +1,29 @@
+import urllib2
+import sys
+from BeautifulSoup import BeautifulSoup
+from pandas import Series, DataFrame
+from tqdm import tqdm
+sys.stdout = open('IMDB_Top_250.txt','w')
+opener = urllib2.build_opener()
+opener.addheaders=[('User-agent','Mozilla/5.0')]
+url = "file:///F:/Coding/Data_Science/IMDB_Scrape_2016/IMDB_Top_250.html"
+ourUrl = opener.open(url).read()
+soup = BeautifulSoup(ourUrl)
+
+table = soup.find('table', attrs={'data-caller-name':'chart-top250movie'})
+table_tbody = table.findChildren('tbody', attrs={'class':'lister-list'})
+count = 1
+itermovie = iter(table_tbody[0].findChildren('tr'))
+
+print 'IMDB Movie List Top 250: '
+
+i = 1
+next(itermovie)
+for tr in tqdm(itermovie):
+    title = tr.find('td', attrs={'class':'titleColumn'})
+    title_info = title.findAll('a')
+    for each in title_info:
+        print str(i) + '. ' + str(title_info[0].text.encode('utf-8').decode('ascii', 'ignore')) + ' - ',
+    rating = tr.findChildren('strong')
+    print str(rating[0].text.encode('utf-8').decode('ascii', 'ignore'))
+    i += 1
